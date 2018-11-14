65d51a4d2c3d393e43ec065b6ef1d3a058c27fb0
Umpoy <iumpoy@yahoo.com>
2018-11-13 20:44:17 -0800

    binary function now returns 8 digit numbers

--- conversion/binary.js
+++ conversion/binary.js
@@ -1,8 +1,9 @@
 function getBinary(string) {
     var split = string.split('');
     var toUnicode = split.map(index => index.charCodeAt(0));
-    var toBinary = toUnicode.map(index => Number(index).toString(2)).join(' ');
-    return toBinary
+    var toBinary = toUnicode.map(index => Number(index).toString(2));
+    toBinary[0] = "0" + toBinary[0]
+    return toBinary.join(' 0')
 }
 
-console.log(getBinary('hello'));
+console.log(getBinary('helloa'));
