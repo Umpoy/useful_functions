f46452c15deea7635fa77a0cdf965da995829849
Umpoy <iumpoy@yahoo.com>
2018-11-18 18:50:22 -0800

    added mutations

---
+++ freecodecamp/mutations.js
@@ -0,0 +1,39 @@
+// Return true if the string in the first element of the array contains all of the letters of the string in the second element of the array.
+
+// For example, ["hello", "Hello"], should return true because all of the letters in the second string are present in the first, ignoring case.
+
+// The arguments ["hello", "hey"] should return false because the string "hello" does not contain a "y".
+
+// Lastly, ["Alien", "line"], should return true because all of the letters in "line" are present in "Alien".
+
+// mutation(["hello", "hey"]) should return false.
+// mutation(["hello", "Hello"]) should return true.
+// mutation(["zyxwvutsrqponmlkjihgfedcba", "qrstu"]) should return true.
+// mutation(["Mary", "Army"]) should return true.
+// mutation(["Mary", "Aarmy"]) should return true.
+// mutation(["Alien", "line"]) should return true.
+// mutation(["floor", "for"]) should return true.
+// mutation(["hello", "neo"]) should return false.
+// mutation(["voodoo", "no"]) should return false.
+
+function mutation(arr) {
+    const str1 = arr[0].toLowerCase();
+    const str2 = arr[1].toLowerCase();
+    for (let i = 0; i < str2.length; i++) {
+        if (!str1.includes(str2[i])) {
+            return false
+        }
+    }
+    return true;
+}
+
+
+console.log(mutation(["hello", "hey"])); // false
+console.log(mutation(["hello", "Hello"])); // true
+console.log(mutation(["zyxwvutsrqponmlkjihgfedcba", "qrstu"])); //true
+console.log(mutation(["Mary", "Army"])); // true
+console.log(mutation(["Mary", "Aarmy"])); // true
+console.log(mutation(["Alien", "line"])); // true
+console.log(mutation(["floor", "for"])); // true
+console.log(mutation(["hello", "neo"])); // false
+console.log(mutation(["voodoo", "no"])); // false
