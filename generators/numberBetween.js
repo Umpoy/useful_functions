7f8754803b91702b5fc9fe7127651ec6f8e9c365
Ian <iumpoy@yahoo.com>
2019-11-19 21:01:51 -0800

    fixed issue were min number will never be logged

--- generators/numberBetween.js
+++ generators/numberBetween.js
@@ -1,3 +1,5 @@
-const numberBetween = (min, max) => Math.ceil(Math.random() * (max - min) + min);
+const numberBetween = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 
 console.log(numberBetween(50, 100));
+console.log(numberBetween(1, 1000));
+console.log(numberBetween(1, 10));
